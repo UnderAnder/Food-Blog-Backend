e134b680de42b4038e61ee837c45182bebba4e37
UnderAnder <andryushin.alexander@gmail.com>
2021-04-13 23:32:52 +0700

    Stage 2/5: Time for recipes

--- blog.py
+++ blog.py
@@ -9,6 +9,11 @@ class DB:
 
     def create_tables(self):
         cur = self.conn.cursor()
+        cur.execute('CREATE TABLE IF NOT EXISTS recipes('
+                    'recipe_id INTEGER PRIMARY KEY,'
+                    'recipe_name TEXT NOT NULL,'
+                    'recipe_description TEXT)'
+                    ';')
         cur.execute('CREATE TABLE IF NOT EXISTS meals('
                     'meal_id INTEGER PRIMARY KEY,'
                     'meal_name TEXT UNIQUE NOT NULL)'
@@ -30,8 +35,15 @@ class DB:
                 cur.execute(f'INSERT INTO {k}({k[:-1]}_name) VALUES (?);', (v,))
         self.conn.commit()
 
+    def add_recipe(self, recipe):
+        cur = self.conn.cursor()
+        print(recipe)
+        cur.execute('INSERT INTO recipes(recipe_name, recipe_description) VALUES (?, ?);', recipe)
+        self.conn.commit()
+
     def close(self):
         self.conn.close()
+
 
 def main():
     db_name = argv[1] if len(argv) == 2 else 'food_blog.db'
@@ -40,7 +52,13 @@ def main():
             "ingredients": ("milk", "cacao", "strawberry", "blueberry", "blackberry", "sugar"),
             "measures": ("ml", "g", "l", "cup", "tbsp", "tsp", "dsp", "")}
     db.populate_tables(data)
-    db.close()
+    while True:
+        recipe_name = input('Recipe name: ')
+        if not recipe_name:
+            db.close()
+            exit()
+        recipe_description = input('Recipe description: ')
+        db.add_recipe((recipe_name, recipe_description))
 
 
 if __name__ == '__main__':
