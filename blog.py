98af4e1a8751ef757649945229039624b893ff9f
UnderAnder <andryushin.alexander@gmail.com>
2021-04-14 17:48:47 +0700

    Stage 3/5: Many-to-many relations

--- blog.py
+++ blog.py
@@ -9,23 +9,33 @@ class DB:
 
     def create_tables(self):
         cur = self.conn.cursor()
+        cur.execute('PRAGMA foreign_keys = ON;')
         cur.execute('CREATE TABLE IF NOT EXISTS recipes('
                     'recipe_id INTEGER PRIMARY KEY,'
                     'recipe_name TEXT NOT NULL,'
-                    'recipe_description TEXT)'
-                    ';')
+                    'recipe_description TEXT'
+                    ');')
         cur.execute('CREATE TABLE IF NOT EXISTS meals('
                     'meal_id INTEGER PRIMARY KEY,'
-                    'meal_name TEXT UNIQUE NOT NULL)'
-                    ';')
+                    'meal_name TEXT UNIQUE NOT NULL'
+                    ');')
         cur.execute('CREATE TABLE IF NOT EXISTS ingredients('
                     'ingredient_id INTEGER PRIMARY KEY,'
-                    'ingredient_name TEXT UNIQUE NOT NULL)'
-                    ';')
+                    'ingredient_name TEXT UNIQUE NOT NULL'
+                    ');')
         cur.execute('CREATE TABLE IF NOT EXISTS measures('
                     'measure_id INTEGER PRIMARY KEY,'
-                    'measure_name TEXT UNIQUE)'
-                    ';')
+                    'measure_name TEXT UNIQUE'
+                    ');')
+        cur.execute('CREATE TABLE IF NOT EXISTS serve('
+                    'serve_id INTEGER PRIMARY KEY,'
+                    'recipe_id INTEGER NOT NULL,'
+                    'meal_id INTEGER NOT NULL,'
+                    'FOREIGN KEY (recipe_id)'
+                    'REFERENCES recipes (recipe_id)'
+                    'FOREIGN KEY (meal_id)'
+                    'REFERENCES meals (meal_id)' 
+                    ');')
         self.conn.commit()
 
     def populate_tables(self, data: dict):
@@ -35,14 +45,29 @@ class DB:
                 cur.execute(f'INSERT INTO {k}({k[:-1]}_name) VALUES (?);', (v,))
         self.conn.commit()
 
-    def add_recipe(self, recipe):
+    def add_recipe(self, recipe) -> int:
         cur = self.conn.cursor()
-        print(recipe)
-        cur.execute('INSERT INTO recipes(recipe_name, recipe_description) VALUES (?, ?);', recipe)
+        result = cur.execute('INSERT INTO recipes(recipe_name, recipe_description) VALUES (?, ?);', recipe).lastrowid
         self.conn.commit()
+        return result
 
     def close(self):
         self.conn.close()
+
+    def print_meals(self):
+        cur = self.conn.cursor()
+        cur.execute('SELECT * FROM meals;')
+        all_meals = cur.fetchall()
+        for meal in all_meals:
+            print(f'{meal[0]}) {meal[1]}', end='  ')
+        print()
+
+    def serve_time(self, recipe_id: int):
+        cur = self.conn.cursor()
+        meals_ids = map(int, input('When the dish can be served: ').split())
+        for i in meals_ids:
+            cur.execute(f'INSERT INTO serve(recipe_id, meal_id) VALUES (?, ?);', (recipe_id, i))
+        self.conn.commit()
 
 
 def main():
@@ -52,13 +77,16 @@ def main():
             "ingredients": ("milk", "cacao", "strawberry", "blueberry", "blackberry", "sugar"),
             "measures": ("ml", "g", "l", "cup", "tbsp", "tsp", "dsp", "")}
     db.populate_tables(data)
+    print('Pass the empty recipe name to exit.')
     while True:
         recipe_name = input('Recipe name: ')
         if not recipe_name:
             db.close()
             exit()
         recipe_description = input('Recipe description: ')
-        db.add_recipe((recipe_name, recipe_description))
+        recipe_id = db.add_recipe((recipe_name, recipe_description))
+        db.print_meals()
+        db.serve_time(recipe_id)
 
 
 if __name__ == '__main__':
