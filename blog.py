576d2b9deb347e806f529595d0bb62d9d46d169f
UnderAnder <andryushin.alexander@gmail.com>
2021-04-16 17:51:49 +0700

    Stage 5/5: First interface

--- blog.py
+++ blog.py
@@ -53,7 +53,7 @@ class DB:
                 cur.execute(f'INSERT INTO {k}({k[:-1]}_name) VALUES (?);', (v,))
         self.conn.commit()
 
-    def add_recipe(self, recipe) -> int:
+    def add_recipe(self, recipe: tuple) -> int:
         cur = self.conn.cursor()
         result = cur.execute('INSERT INTO recipes(recipe_name, recipe_description) VALUES (?, ?);', recipe).lastrowid
         self.conn.commit()
@@ -96,7 +96,6 @@ class DB:
             else:
                 print('Wrong lenght!')
                 continue
-
             cur.execute('INSERT INTO quantity(measure_id, ingredient_id, recipe_id, quantity) '
                         'VALUES (?, ?, ?, ?);', (measure_id, ingredient_id, recipe_id, int(quantity)))
             self.conn.commit()
@@ -104,26 +103,51 @@ class DB:
     def get_from_table(self, q: str, field: str):
         cur = self.conn.cursor()
         if q == '' and field == 'measure':
-            res = cur.execute('SELECT (measure_id) FROM measures WHERE measure_name = "";').fetchall()
+            res = cur.execute('SELECT measure_id FROM measures WHERE measure_name = "";').fetchall()
         else:
             res = cur.execute(f"SELECT {field}_id FROM {field}s WHERE {field}_name LIKE '%'||?||'%';", (q,)).fetchall()
         return res[0][0] if len(res) == 1 else False
 
+    def find_recipe(self, ingredients: tuple, meals: tuple):
+        cur = self.conn.cursor()
+        recipe_stmt = f"""SELECT r.recipe_id, recipe_name
+            FROM (SELECT * FROM ingredients WHERE ingredient_name IN ({','.join('?'*len(ingredients))})) i
+            INNER JOIN quantity q ON i.ingredient_id = q.ingredient_id
+            INNER JOIN recipes r ON q.recipe_id = r.recipe_id
+            INNER JOIN serve s ON s.recipe_id = r.recipe_id
+            INNER JOIN (SELECT * FROM meals WHERE meal_name IN ({','.join('?'*len(meals))})) m
+            ON m.meal_id = s.meal_id
+            GROUP BY r.recipe_id 
+            HAVING COUNT(DISTINCT i.ingredient_id) = ?
+        """
+        res = cur.execute(recipe_stmt, (*ingredients, *meals, len(ingredients))).fetchall()
+        if not res:
+            print('no such recipes')
+        else:
+            output = ', '.join([el[1] for el in res])
+            print(output)
+
 
 def main():
-    db_name = argv[1] if len(argv) == 2 else 'food_blog.db'
-    db = DB(db_name)
+    db_name = argv[1] if len(argv) >= 2 else 'food_blog.db'
     data = {"meals": ("breakfast", "brunch", "lunch", "supper"),
             "ingredients": ("milk", "cacao", "strawberry", "blueberry", "blackberry", "sugar"),
             "measures": ("ml", "g", "l", "cup", "tbsp", "tsp", "dsp", "")}
-    db.populate_tables(data)
-    print('Pass the empty recipe name to exit.')
-    while recipe_name := input('Recipe name: '):
-        recipe_description = input('Recipe description: ')
-        recipe_id = db.add_recipe((recipe_name, recipe_description))
-        db.print_meals()
-        db.mealtime(recipe_id)
-        db.ingredients_quantity(recipe_id)
+    db = DB(db_name)
+    if len(argv) == 4:
+        ingredients = tuple(argv[2].split('=')[1].strip('"').split(','))
+        meals = tuple(argv[3].split('=')[1].strip('"').split(','))
+        print(ingredients, meals)
+        db.find_recipe(ingredients, meals)
+    else:
+        db.populate_tables(data)
+        print('Pass the empty recipe name to exit.')
+        while recipe_name := input('Recipe name: '):
+            recipe_description = input('Recipe description: ')
+            recipe_id = db.add_recipe((recipe_name, recipe_description))
+            db.print_meals()
+            db.mealtime(recipe_id)
+            db.ingredients_quantity(recipe_id)
     db.conn.close()
 
 
